422c311c44ea89c3d3a0bd513f542c84c12c087f
tejasj <tejasj@mit.edu>
2021-03-28 21:49:10 -0400

    Update code_bp_torch.py

--- bp/torch_parallel/code_bp_torch.py
+++ bp/torch_parallel/code_bp_torch.py
@@ -148,7 +148,9 @@ class CodeBP(nn.Module):
         nan_check = torch.logical_and(torch.isnan(M_out_diff), torch.isinf(self.Hxs[factor_neighbors, grid]))
         nan_check_idx = torch.nonzero(torch.sum(nan_check.float(), -1, keepdim=True))
         # Resolve nans by calculating meaningful messages
-        if nan_check.shape[0] > 0:
+        if nan_check_idx.shape[0] > 0:
+            print("here", nan_check_idx.shape)
+            print(M_out_diff[nan_check_idx[:,0], 0])
             M_out_diff[nan_check_idx[:,0], 0] = \
                 torch.tanh(torch.sum(self.Hxs.data[factor_neighbors, grid].masked_fill(nan_check, 0), -1, keepdim=True))[nan_check_idx[:,0],0]
         # Convert likelihoods back to probability
