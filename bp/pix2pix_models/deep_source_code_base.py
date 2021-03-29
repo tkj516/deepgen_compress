4e78832d6744e222c2b6131dd3907f77783832cf
tejasj <tejasj@mit.edu>
2021-03-28 20:14:25 -0400

    Update deep_source_code_base.py

--- bp/pix2pix_models/deep_source_code_base.py
+++ bp/pix2pix_models/deep_source_code_base.py
@@ -41,37 +41,24 @@ class BaseModel(ABC):
         self.image_paths = []
         self.metric = 0  # used for learning rate policy 'plateau'
 
-    @staticmethod
-    def modify_commandline_options(parser, is_train):
-        """Add new model-specific options, and rewrite default values for existing options.
+    # @abstractmethod
+    # def set_input(self, input):
+    #     """Unpack input data from the dataloader and perform necessary pre-processing steps.
 
-        Parameters:
-            parser          -- original option parser
-            is_train (bool) -- whether training phase or test phase. You can use this flag to add training-specific or test-specific options.
+    #     Parameters:
+    #         input (dict): includes the data itself and its metadata information.
+    #     """
+    #     pass
 
-        Returns:
-            the modified parser.
-        """
-        return parser
+    # @abstractmethod
+    # def forward(self):
+    #     """Run forward pass; called by both functions <optimize_parameters> and <test>."""
+    #     pass
 
-    @abstractmethod
-    def set_input(self, input):
-        """Unpack input data from the dataloader and perform necessary pre-processing steps.
-
-        Parameters:
-            input (dict): includes the data itself and its metadata information.
-        """
-        pass
-
-    @abstractmethod
-    def forward(self):
-        """Run forward pass; called by both functions <optimize_parameters> and <test>."""
-        pass
-
-    @abstractmethod
-    def optimize_parameters(self):
-        """Calculate losses, gradients, and update network weights; called in every training iteration"""
-        pass
+    # @abstractmethod
+    # def optimize_parameters(self):
+    #     """Calculate losses, gradients, and update network weights; called in every training iteration"""
+    #     pass
 
     def setup(self, opt):
         """Load and print networks; create schedulers
