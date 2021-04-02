9860534e559492ad2e3445e0ce79f4e27d9dc070
tejasj <tejasj@mit.edu>
2021-04-02 11:24:33 -0400

    Update deep_source_code_pixelcnnpp.py

--- bp/deep_source_code_pixelcnnpp.py
+++ bp/deep_source_code_pixelcnnpp.py
@@ -245,6 +245,7 @@ class SourceCodeBP():
         self.M_from_code = self.code.M_out
         # Reshape to send to grid
         self.M_to_grid = self.M_from_code.reshape(self.h, self.w, 2)
+        print(self.M_to_grid)
 
         # Perform one step of source graph belief propagation
         # Extract the last channel of the code message
@@ -252,7 +253,6 @@ class SourceCodeBP():
         belief /= torch.sum(belief, -1, keepdim=True)
         source_input = belief[:,:,1].reshape(1, 1, self.h, self.w)
         self.M_from_grid = self.source.message(source_input)
-        print(self.M_from_grid)
         # Permute this output
         self.M_from_grid = self.M_from_grid.squeeze(0).permute(1, 2, 0)
         # Reshape to send to code
