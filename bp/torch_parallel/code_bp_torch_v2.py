f5a033af8f00c79091e70bc6961bd66e6e391e71
tejasj <tejasj@mit.edu>
2021-04-03 17:29:58 -0400

    Update code_bp_torch_v2.py

--- bp/torch_parallel/code_bp_torch_v2.py
+++ bp/torch_parallel/code_bp_torch_v2.py
@@ -39,8 +39,13 @@ class CodeBP(nn.Module):
         print("[Building adjacency list ...]")
 
         # Intialize the lists
-        self.neighbors_of_factor = [[] for _ in range(self.K)]
+        self.neighbors_of_factor = [[self.N] for _ in range(self.K)]
         self.neighbors_of_variable = [[] for _ in range(self.N)]
+
+        # Keep track if unequal variable neighbors
+        # Create a temporary column to attend in this case
+        self.unequal_neighbors = self.K
+        self.temp_column = torch.zeros(self.K, 1)
 
         # Create a sparse matrix
         sp_H = csr_matrix(self.H.cpu().numpy())
@@ -50,6 +55,10 @@ class CodeBP(nn.Module):
         for a, i in tqdm(zip(rows, cols)):
             self.neighbors_of_variable[i].append(a)
             self.neighbors_of_factor[a].append(i)
+            if len(self.neighbors_of_factor[a]) == self.max_variable_neighbors + 1:
+                self.neighbors_of_factor[a].pop(0)
+                self.unequal_neighbors -= 1
+                self.temp_column[a] = float('inf')
 
     def forward(self, ps, x, Min):
         """
@@ -115,6 +124,10 @@ class CodeBP(nn.Module):
         grid = torch.LongTensor([[i]*self.max_variable_neighbors for i in range(self.K)]).to(self.device)
         variable_neighbors = torch.LongTensor(self.neighbors_of_factor).to(self.device)
 
+        # Append temporary column if unequal number of variable neighbors per factor node
+        if self.unequal_neighbors > 0:
+            self.Hsx.data = torch.cat([self.Hsx.data, self.temp_column], 1)
+
         ll_diff_prod = 1 - 2*x
         # Calculate all the log-likelihood terms that will be multiplied at each factor
         ll_diff = torch.tanh(self.Hsx.data[grid, variable_neighbors])
@@ -142,11 +155,15 @@ class CodeBP(nn.Module):
         if check.shape[0] > 0:
             self.Hxs.data[grid[check[:,0], :], variable_neighbors[check[:,0], :]] = torch.atanh(ll_diff_prod[check[:,0]] / ll_diff[check[:,0], :])
 
+        # Remove the added column if used
+        if self.unequal_neighbors > 0:
+            self.Hsx.data = self.Hsx.data[:, :-1]
+
         #####################################################################################################
         # Outgoing super edge messages
         #####################################################################################################
 
-        grid = torch.LongTensor([[i]*3 for i in range(self.N)]).to(self.device)
+        grid = torch.LongTensor([[i]*self.max_factor_neighbors for i in range(self.N)]).to(self.device)
         # Sum up all the incoming messages at each node and apply tanh to get likelihood
         M_out_diff = torch.tanh(torch.sum(self.Hxs.data[factor_neighbors, grid], -1, keepdim=True))
         nan_check = torch.logical_and(torch.isnan(M_out_diff), torch.isinf(self.Hxs[factor_neighbors, grid]))
