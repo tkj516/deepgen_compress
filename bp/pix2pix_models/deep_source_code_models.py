1a8ef23fb4af3019166588a43c614f66f62e2015
tejasj <tejasj@mit.edu>
2021-04-06 16:21:19 -0400

    Update deep_source_code_models.py

--- bp/pix2pix_models/deep_source_code_models.py
+++ bp/pix2pix_models/deep_source_code_models.py
@@ -106,13 +106,14 @@ class DeepSourceCode(BaseModel):
         # Reshape to send to grid
         self.M_to_grid = self.M_from_code.reshape(self.h, self.w, 2)
 
+        print(self.M_to_grid)
         # Modify the message for input to the source network
         intermediate_B = self.M_to_grid * self.npot
         intermediate_B /= torch.sum(intermediate_B, -1, keepdim=True)
         source_input = intermediate_B[..., 1].reshape(1, 1, self.h, self.w).float()
+        print(source_input)
 
         # Perform one step of source graph belief propagation
-        print(source_input)
         self.M_from_grid = torch.sigmoid(self.source(source_input, self.x.reshape(-1, 1, self.h//4, self.w//4))).reshape(1, 1)
         # Reshape to send to code
         self.M_to_code = torch.cat([(1-self.M_from_grid)*torch.ones(self.N, 1).to(self.device), self.M_from_grid*torch.ones(self.N, 1).to(self.device)], -1)
