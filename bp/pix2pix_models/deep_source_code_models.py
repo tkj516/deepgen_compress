ce6403cec63252625f4a32e94d238d21130ca8f6
tejasj <tejasj@mit.edu>
2021-04-06 16:37:29 -0400

    Update deep_source_code_models.py

--- bp/pix2pix_models/deep_source_code_models.py
+++ bp/pix2pix_models/deep_source_code_models.py
@@ -129,11 +129,8 @@ class DeepSourceCode(BaseModel):
             self.decode_step()
 
             # Calculate the belief
-            print(self.M_from_grid)
-            print(self.M_to_grid)
             self.B = self.M_from_grid * self.M_to_grid * self.npot
             self.B /= torch.sum(self.B, -1).unsqueeze(-1)
-            print(self.B)
 
             # Termination condition to end belief propagation
             if torch.sum(torch.abs(self.B[..., 1] - B_old)).item() < 0.5:
