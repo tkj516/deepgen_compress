c3a6bc515909ab5c667964fd31d4081b1bb2094a
tejasj <tejasj@mit.edu>
2021-04-06 23:13:19 -0400

    Update deep_source_code_pixelcnnpp_mnist.py

--- bp/deep_source_code_pixelcnnpp_mnist.py
+++ bp/deep_source_code_pixelcnnpp_mnist.py
@@ -264,7 +264,7 @@ class SourceCodeBP():
         # Extract the last channel of the code message
         belief = self.M_to_grid*self.npot
         belief /= torch.sum(belief, -1, keepdim=True)
-        source_input = belief[:,:,1].reshape(1, 1, self.h, self.w)
+        source_input = (belief[:,:,1].reshape(1, 1, self.h, self.w) > 0.5).float()
         print(source_input)
         self.M_from_grid = self.source.message(source_input)
         # Permute this output
