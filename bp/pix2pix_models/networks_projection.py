a46cab6a6c5dd7ada578910947882e2d8cdf8269
tejasj <tejasj@mit.edu>
2021-04-19 16:07:03 -0400

    Update networks_projection.py

--- bp/pix2pix_models/networks_projection.py
+++ bp/pix2pix_models/networks_projection.py
@@ -4,7 +4,7 @@ from torch.nn import init
 import functools
 from torch.optim import lr_scheduler
 
-from isinggan_arch import *
+from .isinggan_arch import *
 from basicsr.models.archs.discriminator_arch import VGGStyleDiscriminator128
 
 ###############################################################################
