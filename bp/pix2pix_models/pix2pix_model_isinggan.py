0c9954130a01fa80ade8eef1a41d2a2ece7cf190
tejasj <tejasj@mit.edu>
2021-04-19 19:21:24 -0400

    Update pix2pix_model_isinggan.py

--- bp/pix2pix_models/pix2pix_model_isinggan.py
+++ bp/pix2pix_models/pix2pix_model_isinggan.py
@@ -83,7 +83,7 @@ class Pix2PixModel(BaseModel):
         
         # Get the fake logits segmentation mask
         # input = torch.rand(self.b, self.c*self.h//4*self.w//4).to(self.device)
-        input = torch.randn(selfb, 100)
+        input = torch.randn(self.b, 100)
         self.fake_B = self.netG(input)  # G(A)
 
     def backward_D(self):
