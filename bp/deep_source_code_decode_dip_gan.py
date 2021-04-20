7326632c05144e901a65930dff5aa3ad475c0682
tejasj <tejasj@mit.edu>
2021-04-20 01:27:14 -0400

    Update deep_source_code_decode_dip_gan.py

--- bp/deep_source_code_decode_dip_gan.py
+++ bp/deep_source_code_decode_dip_gan.py
@@ -125,7 +125,7 @@ def test_source_code_decode():
     decoder = Decoder(H).to(device)
 
     # Setup an optimizer for the input image
-    optimizer = torch.optim.Adam(params=decoder.massager.parameters(), lr=2e-4, betas=(0.9, 0.999))
+    optimizer = torch.optim.Adam(params=decoder.massager.parameters(), lr=1e-3, betas=(0.9, 0.999))
 
     # Either load a sample image or generate one using Gibb's sampling
     print("[Generating the sample ...]")
