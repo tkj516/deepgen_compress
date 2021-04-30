c20f0e30beb962ec41da0d3f4942f9351aa1dd18
tejasj <tejasj@mit.edu>
2021-04-30 03:52:05 -0400

    Update dgcspn_source_code_bp.py

--- bp/dgcspn_source_code_bp.py
+++ bp/dgcspn_source_code_bp.py
@@ -223,7 +223,7 @@ class SourceCodeBP():
         self.samp, _ = self.dataset[idx]
         self.samp = torch.FloatTensor(self.samp.reshape(-1, 1)).to(device)
 
-        self.samp = (torch.rand(1, 1, 28, 28) > 0.5).float().to(device)
+        self.samp = (torch.rand(28, 28) > 0.5).float().reshape(-1, 1).to(device)
 
     def encode(self):
 
