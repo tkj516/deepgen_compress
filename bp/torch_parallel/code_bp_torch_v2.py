816d62af6a9512fff9523505eecc0c8cc1bb8839
tejasj <tejasj@mit.edu>
2021-04-30 02:10:24 -0400

    Update code_bp_torch_v2.py

--- bp/torch_parallel/code_bp_torch_v2.py
+++ bp/torch_parallel/code_bp_torch_v2.py
@@ -173,14 +173,13 @@ class CodeBP(nn.Module):
         grid = torch.LongTensor([[i]*self.max_factor_neighbors for i in range(self.N)]).to(self.device)
         # Sum up all the incoming messages at each node and apply tanh to get likelihood
         M_out_diff = torch.tanh(torch.sum(self.Hxs.data[factor_neighbors, grid], -1, keepdim=True))
-        print(M_out_diff)
         nan_check = torch.logical_and(torch.isnan(M_out_diff), torch.isinf(self.Hxs[factor_neighbors, grid]))
         nan_check_idx = torch.nonzero(torch.sum(nan_check.float(), -1, keepdim=True))
         # Resolve nans by calculating meaningful messages
         if nan_check_idx.shape[0] > 0:
+            print("here")
             M_out_diff[nan_check_idx[:,0], 0] = \
                 torch.tanh(torch.sum(self.Hxs.data[factor_neighbors, grid].masked_fill(nan_check, 0), -1, keepdim=True))[nan_check_idx[:,0],0]
-        print(M_out_diff)
         # Convert likelihoods back to probability
         self.M_out.data = 0.5 + torch.cat([M_out_diff, -M_out_diff], -1)/2
 
