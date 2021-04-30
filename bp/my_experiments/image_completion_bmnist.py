8e637b51fdb605240cc0553cc9f24b59afa7e241
tejasj <tejasj@mit.edu>
2021-04-30 10:45:28 -0400

    Update image_completion_bmnist.py

--- bp/my_experiments/image_completion_bmnist.py
+++ bp/my_experiments/image_completion_bmnist.py
@@ -188,7 +188,7 @@ if __name__ == '__main__':
         sample, _ = model.mpe(x)
 
         image_grid = torchvision.utils.make_grid(torch.cat([orig, 
-                                                 torch.where(torch.isnan(x.cpu(), torch.tensor(0.5), x.cpu())), 
+                                                 torch.where(torch.isnan(x.cpu()), torch.tensor(0.5), x.cpu()), 
                                                  sample.cpu()], dim=0))
         writer.add_image(f'test/{count}_{omittion_order[0]}', image_grid, count)
 
