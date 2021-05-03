079d54484e016691617e6c5d44038157b5cdc08c
tejasj <tejasj@mit.edu>
2021-05-02 20:43:47 -0400

    Update dgcspn_source_code_bp_ising.py

--- bp/dgcspn_source_code_bp_ising.py
+++ bp/dgcspn_source_code_bp_ising.py
@@ -193,7 +193,7 @@ class Source():
         # If nan then this was doped pixel, replace nan with 0 prob => -inf log prob
         message = z_grad.squeeze(0).reshape(2, 784).permute(1, 0)
         message = torch.where(torch.isnan(message), torch.tensor(float('-inf')).to(device), message)
-        message -= torch.logsumexp(message, dim=-1, keepdim=True)
+        # message -= torch.logsumexp(message, dim=-1, keepdim=True)
         print(message)
 
         return message
