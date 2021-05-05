9dd29d88a5eb65d181f7a7bcd5c57103bbb5a5b9
tejasj <tejasj@mit.edu>
2021-05-05 00:45:25 -0400

    Update gen_ising.sh

--- bp/gen_ising.sh
+++ bp/gen_ising.sh
@@ -2,5 +2,5 @@
 
 for i in {0..0.95..0.05};
   do 
-     python gen_ising_dataset.py --p 0.5 --stay $i --num_images 200000 --root ising_0.5_$i_200000
-& done
+     python gen_ising_dataset.py --p 0.5 --stay $i --num_images 200000 --root ising_0.5_$i_200000 &
+done
