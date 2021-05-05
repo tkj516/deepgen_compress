0d6935c79895386657cfd3989478dbc9bb684b04
tejasj <tejasj@mit.edu>
2021-05-05 00:49:20 -0400

    Update gen_ising.sh

--- bp/gen_ising.sh
+++ bp/gen_ising.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {0..0.95..0.05};
+for i in $(seq 0.05 .05 0.95);
   do 
      python gen_ising_dataset.py --p 0.5 --stay $i --num_images 200000 --root ising_0.5_$i_200000 &
 done
