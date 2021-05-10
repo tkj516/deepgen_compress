750328adbce86e65322da0fa6eefc1530e70757c
tejasj <tejasj@mit.edu>
2021-05-10 13:38:45 -0400

    Updates

--- bp/spn_code_decoding/demo.py
+++ bp/spn_code_decoding/demo.py
@@ -222,14 +222,13 @@ class Demo():
         results['min_rate'] = min_rate
         results['max_rate'] = max_rate
 
-        os.makedirs('temp.json', exist_ok=True)
         with open('temp.json', 'w') as file:
             json.dump(results, file, indent=4)
 
         print(f'Avg Rate: {avg_rate/100.0}, Min Rate: {min_rate/100.0}, Max Rate: {max_rate/100.0}')
 
         filepath = os.path.join('demo', args.dataset, args.source_type + '_' + args.phase, os.path.basename(args.root_dir).split('.')[0] + '.json')
-        os.makedirs(filepath, exist_ok=True)
+        os.makedirs(os.path.join('demo', args.dataset, args.source_type + '_' + args.phase), exist_ok=True)
         with open(filepath, 'w') as file:
             json.dump(results, file, indent=4)
 
--- bp/spn_code_decoding/demo.sh
+++ bp/spn_code_decoding/demo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python demo.py --binary --depthwise --dataset mnist --root_dir ../../../MNIST --phase test --source_type spn --checkpoint dgcspn/mnist/generative/model_2021-05-06_11:46:44.pt
+python demo.py --binary --depthwise --dataset mnist --root_dir ../../../MNIST --phase test --source_type spn --checkpoint dgcspn/mnist/generative/model_2021-05-06_11:46:44.pt --num_avg 100
 
