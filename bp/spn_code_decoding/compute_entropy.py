0b80891f3aa3feca305edcd149d16bb57c98975f
tejasj <tejasj@mit.edu>
2021-05-27 00:58:11 -0400

    Update compute_entropy.py

--- bp/spn_code_decoding/compute_entropy.py
+++ bp/spn_code_decoding/compute_entropy.py
@@ -134,8 +134,6 @@ def compute_sep_threshold(sep_prot, doperate=0.04):
         
         while maximum >= 0:
 
-            print(maximum)
-
             eps -= 0.005
             maximum = np.max(f(eps, x) - x)
 
@@ -145,26 +143,28 @@ def compute_sep_threshold(sep_prot, doperate=0.04):
 
 if __name__ == "__main__":
 
-    # hs, stays = ising_entropy()
+    hs, stays = ising_entropy()
 
-    # hf, newstays = finite_entropy(np.arange(0.5, 1.0, 0.04), N=50)
+    hf, newstays = finite_entropy(np.arange(0.5, 1.0, 0.04), N=50)
 
     q = [0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95]
 
-    pgm = [1.19649, 1.18292, 1.14193, 1.03935, 0.71966, 0.33407, 0.20538, 0.16562, 0.1470, 0.14261]
-    spn = [1.19490, 1.18673, 1.14894, 1.06529, 0.74532, 0.34625, 0.22401, 0.17977, 0.16053, 0.15176]
+    sep_prot_pgm = [1.19649, 1.18292, 1.14193, 1.03935, 0.71966, 0.33407, 0.20538, 0.16562, 0.1470, 0.14261]
+    sep_prot_spn = [1.19490, 1.18673, 1.14894, 1.06529, 0.74532, 0.34625, 0.22401, 0.17977, 0.16053, 0.15176]
 
-    # fig, ax = plt.subplots()
-    # ax.plot(stays, hs, 'b-', label='H_inf')
-    # ax.plot(newstays, hf, 'r-', label='H_f')
-    # ax.plot(q, pgm, '+-', label='pgm')
-    # ax.plot(q, spn, 'x-', label='spn')
-    # ax.set_ylabel('output bits per input bits')
-    # ax.set_xlabel('q')
-    # ax.legend()
+    sep_threshold_pgm = compute_sep_threshold(sep_prot_pgm)
+    sep_threshold_spn = compute_sep_threshold(sep_prot_spn)
 
-    # plt.grid()
-    # plt.show()
+    fig, ax = plt.subplots()
+    ax.plot(stays, hs, 'b-', label='$H_\infty$')
+    ax.plot(newstays, hf, 'r-', label='$H_{h, w}$')
+    ax.plot(q, sep_prot_pgm, '+-', label='sep-prot PGM')
+    ax.plot(q, sep_prot_spn, 'x-', label='sep-prot SPN')
+    ax.plot(q, sep_threshold_pgm, 'o-', label='sep-thresh PGM')
+    ax.plot(q, sep_threshold_spn, '^-', label='sep-thresh SPN')
+    ax.set_ylabel('output bits per input bits')
+    ax.set_xlabel('q')
+    ax.legend()
 
-    sep_threshold_pgm = compute_sep_threshold(pgm)
-    print(sep_threshold_pgm)
+    plt.grid()
+    plt.show()
