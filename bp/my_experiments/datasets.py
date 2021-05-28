fc09ea948d25d04aa68f254a7e285d39be230f5c
tejasj <tejasj@mit.edu>
2021-05-28 10:41:07 -0400

    Added training script for uci-dna

--- bp/my_experiments/datasets.py
+++ bp/my_experiments/datasets.py
@@ -39,3 +39,56 @@ class IsingDataset(Dataset):
         sample = torch.FloatTensor(image).unsqueeze(0)
 
         return sample, torch.tensor([0])
+
+def csv_to_numpy(filepath, sep=',', dtype=np.uint8):
+    with open(filepath, "r") as file:
+        reader = csv.reader(file, delimiter=sep)
+        dataset = np.array(list(reader)).astype(dtype)
+        return dataset
+
+def load_binary_dataset(root, name):
+    directory = os.path.join(root, 'binary', name)
+    data_train = csv_to_numpy(os.path.join(directory, name + '.train.data'))
+    data_valid = csv_to_numpy(os.path.join(directory, name + '.valid.data'))
+    data_test = csv_to_numpy(os.path.join(directory, name + '.test.data'))
+    return data_train, data_valid, data_test
+
+class UCIDNADataset(Dataset):
+    """Dataset of Gibbs sampled Ising images"""
+
+    def __init__(self, 
+                root='/fs/data/tejasj/Masters_Thesis/spnflow/datasets',
+                name='dna',
+                phase='train', 
+                transform=None):
+
+        # Choose the phase
+        self.phase = phase
+
+        # Get the train, val and test datasets
+        data_train, data_valid, data_test = load_binary_dataset(root, name)
+
+        print(data_train.shape)
+        exit(0)
+
+        # Choose the number of files
+        if self.phase == 'train':
+            start_idx = 0
+            end_idx = int(0.9*len(self.files))
+        else:
+            start_idx = int(0.9*len(self.files))
+            end_idx = len(self.files)
+
+        self.files = sorted(os.listdir(self.root_dir))[start_idx:end_idx]
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+
+        image = os.path.join(self.root_dir, self.files[idx])
+        image = np.load(image)
+
+        sample = torch.FloatTensor(image).unsqueeze(0)
+
+        return sample, torch.tensor([0])
